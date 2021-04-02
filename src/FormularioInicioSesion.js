e66d225bcb42ee967df83570fb78baf83f67c12c
David Aponte <davidxarias321@gmail.com>
2021-04-02 20:22:13 -0300

    Creacion del Projecto y comienzo de la estructura del componente de Formulario de Vuelos

---
+++ src/FormularioInicioSesion.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+
+
+function InicioSesion(props){
+    return (
+    <div>
+        <h1>Icarus Viajes</h1> 
+        
+        <form method="" action="">
+      
+ 
+       
+         
+
+        
+        
+         
+
+        </form>
+
+        <a href="www.google.com" target="_blank ">Registrarse</a>
+
+        </div>
+        )
+}
+
+export default InicioSesion
+
+   
+ 
---
+++ src/PaginaDeInicio.js
@@ -0,0 +1,130 @@
+import React from 'react';
+//  import InicioSesion from './FormularioInicioSesion';
+import './PaginaDeInicio.css';
+
+class PaginaDeInicio extends React.Component{
+  constructor(props){
+  super(props);
+  this.idayVueltaRadioButton = this.idayVueltaRadioButton.bind(this)
+  this.idaRadioButton = this.idaRadioButton.bind(this)
+  this.state = {
+
+  FormularioDeVuelos: 
+  <div>
+    <h2>Vuelos Online</h2> 
+    <h3>Encuentra tus pasajes Aqui</h3>
+    
+    <form action="" method="">
+ 
+ 
+     <label htmlFor="ida">Ida</label>
+      <input type="radio" name="viaje" id="ida" value="Ida" checked ></input>
+ 
+      <label htmlFor="idaYVuelta">Ida y vuelta</label>
+      <input type="radio" name="viaje" id="idaYVuelta" value="IdaYVuelta"  onClick={this.idayVueltaRadioButton}></input>
+    
+      <br></br>
+
+      <laber htmlFor="salida">Salida:</laber>
+     <input type="text" placeholder="Ciudad Origen"  id="salida"></input>
+     <input type="text" placeholder="Ciudad Destino" id="salida"></input>
+     <input type="date" placeholder="Fecha"          id="salida"></input> 
+ 
+     <input type="submit"></input>
+ 
+ 
+      
+    </form>
+    
+    
+    </div>
+}
+
+
+}
+
+idayVueltaRadioButton(){
+    this.setState({
+      FormularioDeVuelos: <div> <h2>Vuelos Online</h2> 
+      <h3>Encuentra tus pasajes Aqui</h3>
+      
+      <form action="" method="">
+   
+   
+       <label htmlFor="ida">Ida</label>
+        <input type="radio" name="viaje" id="ida" value="Ida" onClick={this.idaRadioButton}  ></input>
+   
+        <label htmlFor="idaYVuelta">Ida y vuelta</label>
+        <input type="radio" name="viaje" id="idaYVuelta" value="IdaYVuelta"checked></input>
+
+        <br></br>
+      
+       <laber htmlFor="salida">Salida:</laber>
+       <input type="text" placeholder="Ciudad Origen"  id="salida"></input>
+       <input type="text" placeholder="Ciudad Destino" id="salida"></input>
+       <input type="date" placeholder="Fecha"          id="salida"></input> 
+   
+       <br></br>
+
+       <laber htmlFor="regreso">Regreso:</laber>
+       <input type="text" placeholder="Ciudad Origen"  id="regreso"></input>
+       <input type="text" placeholder="Ciudad Destino" id="regreso"></input>
+       <input type="date" placeholder="Fecha"          id="regreso"></input> 
+   
+
+       <input type="submit"></input>
+   
+   
+        
+      </form>
+      
+      
+      </div>
+
+
+    })
+}
+
+idaRadioButton(){
+
+  this.setState({
+ 
+    FormularioDeVuelos: <div> <h2>Vuelos Online</h2> 
+    <h3>Encuentra tus pasajes Aqui</h3>
+    
+    <form action="" method="">
+ 
+ 
+     <label htmlFor="ida">Ida</label>
+      <input type="radio" name="viaje" id="ida" value="Ida" checked ></input> 
+ 
+      <label htmlFor="idaYVuelta">Ida y vuelta</label>
+      <input type="radio" name="viaje" id="idaYVuelta" value="IdaYVuelta"  onClick={this.idayVueltaRadioButton}></input>
+    
+      <br></br>
+
+      <label htmlFor="salida">Salida:</label>
+     <input type="text" placeholder="Ciudad Origen"  id="salida"></input>
+     <input type="text" placeholder="Ciudad Destino" id="salida"></input>
+     <input type="date" placeholder="Fecha"          id="salida"></input> 
+ 
+     <input type="submit"></input>
+ 
+ 
+      
+    </form>
+    
+    
+    </div>
+
+
+
+  })
+}
+   render (){
+    return (<div>{this.state.FormularioDeVuelos}</div>)
+}
+
+}
+
+export default PaginaDeInicio;
