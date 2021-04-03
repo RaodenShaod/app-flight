a77e318ccb182ef79122384684ac6f52813bfd57
Joan <69653003+Jochizan@users.noreply.github.com>
2021-04-02 21:52:27 -0500

    Refactorización del proyecto para una mejor coordinación

---
+++ src/components/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Homepage = () => {
+  return (
+    <div>
+      <h1>Icarus Viajes</h1>
+
+      <form method='' action=''></form>
+
+      <a href='www.google.com' target='_blank '>
+        Registrarse
+      </a>
+    </div>
+  );
+};
+
+export default Homepage;
