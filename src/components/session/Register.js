00260c28c65708a05341bb54f336c508379ff197
Jochizan <remnyachizot2015@gmail.com>
2021-04-03 08:13:21 -0500

    Creating folder only for managing sessions

--- src/components/session/Register.js
+++ src/components/session/Register.js
@@ -1,17 +1,15 @@
-import React from 'react';
-
-const Homepage = () => {
+const Register = () => {
   return (
     <div>
       <h1>Icarus Viajes</h1>
 
       <form method='' action=''></form>
 
-      <a href='www.google.com' target='_blank '>
+      <a href='/' target='_blank '>
         Registrarse
       </a>
     </div>
   );
 };
 
-export default Homepage;
+export default Register;
